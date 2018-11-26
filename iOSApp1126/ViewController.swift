3b1ed5e9b25c74081f4c7925cdee0fca1e19db9c
503-08 <a503-08@503-08ui-iMac.local>
2018-11-26 10:47:28 +0900

    Initial Commit

---
+++ iOSApp1126/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSApp1126
+//
+//  Created by 503-08 on 26/11/2018.
+//  Copyright © 2018 leewoomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
